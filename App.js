d4e8776d6eec67c9ee28b76d270f607bdd7476f3
ashuashu2 <86846323+ashuashu2@users.noreply.github.com>
2021-12-18 00:56:31 +0530

    update

--- App.js
+++ App.js
@@ -2,47 +2,49 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
 var message = document.querySelector(".error-message");
-var noOfNotes= document.querySelectorAll(".table-d-1");
+var noOfNotes = document.querySelectorAll(".table-d-1");
 
 
-var availableNotes = [2000,500,100,20,10,5,1];
-checkButton.addEventListener("click",function validateBillAndCashAmount() {
-   hideMessage();
+var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
 
-    if (billAmount.value > 0) {  
+    if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             var amountToBeReturned = cashGiven.value - billAmount.value;
-            
-            calculateChange(amountToBeReturned);
-        } else { 
-        
-        showMessage(" Do you wanna wash plates?")
-        }
-    
-} else {
-   
 
-showMessage("invalid bill amount")
-}});
-function calculateChange(amountToBeReturned){
-    for (let i = 0 ; i < availableNotes.length ;  i++){
-        var numberOfNotes = Math.trunc( 
-            amountToBeReturned  / availableNotes[i]
+            calculateChange(amountToBeReturned);
+        } else {
+
+            showMessage(" Do you wanna wash plates?")
+        }
+
+    } else {
+
+
+        showMessage("invalid bill amount")
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        var numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
         )
         amountToBeReturned = amountToBeReturned % availableNotes[i]
-       noOfNotes[i].innerText = numberOfNotes;
+        noOfNotes[i].innerText = numberOfNotes;
     }
 
 }
 
 function hideMessage() {
-    message.style.display = "none" 
+    message.style.display = "none"
 
 }
 
 
 
-function showMessage(msg){
-    message.style.display = "block" 
+function showMessage(msg) {
+    message.style.display = "block"
     message.innerText = msg;
 }
